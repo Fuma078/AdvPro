678ca17bb55fab5014d539e7be9ecc3c33d6c5a8
Fuma Yuki <fam0075552@gmail.com>
2025-09-03 20:51:08 +0700

    Add files via upload

---
+++ Chap4/src/main/java/se233/chapter4/Launcher.java
@@ -0,0 +1,28 @@
+package se233.chapter4;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import se233.chapter4.view.GameStage;
+import se233.chapter4.controller.GameLoop;
+import se233.chapter4.controller.DrawingLoop;
+
+public class Launcher extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+    @Override
+    public void start(Stage stage) {
+        GameStage gameStage = new GameStage();
+        GameLoop gameLoop = new GameLoop(gameStage);
+        DrawingLoop drawingLoop = new DrawingLoop(gameStage);
+        Scene scene = new Scene(gameStage, gameStage.WIDTH, gameStage.HEIGHT);
+        scene.setOnKeyPressed(event -> gameStage.getKeys().add(event.getCode()));
+        scene.setOnKeyReleased(event -> gameStage.getKeys().remove(event.getCode()));
+        stage.setTitle("Mario");
+        stage.setScene(scene);
+        stage.show();
+        (new Thread(gameLoop)).start();
+        (new Thread(drawingLoop)).start();
+    }
+}
